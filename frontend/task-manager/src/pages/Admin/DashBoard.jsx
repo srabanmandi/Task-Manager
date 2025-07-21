bdb910998a6a74162944033c06deb36536b4f71d
srabanmandi <shrabon2812@gmail.com>
2025-07-21 13:47:26 +0530

    Dashboard: Pie and Bar Chart completed

--- frontend/task-manager/src/pages/Admin/DashBoard.jsx
+++ frontend/task-manager/src/pages/Admin/DashBoard.jsx
@@ -10,6 +10,10 @@ import { addThousandSeparator } from "../../utils/helper";
 import InfoCard from "../../components/Cards/InfoCard";
 import { LuArrowRight } from "react-icons/lu";
 import TaskListTable from "../../components/TaskListTable";
+import CustomPieChart from "../../components/Charts/CustomPieChart";
+import CustomBarChart from "../../components/Charts/CustomBarChart";
+
+const COLORS = ["#8D51FF", "#00B8DB", "#7BCE00"];
 
 function DashBoard() {
   useUserAuth();
@@ -22,23 +26,47 @@ function DashBoard() {
   const [pieChartData, setPieChartData] = useState([]);
   const [barCharData, setBarCharData] = useState([]);
 
+  // Prepare Chart Data
+  const prepareChartData = (data) => {
+    const taskDistribution = data?.taskDistribution || null;
+    const taskPriorityLevels = data?.taskPriorityLevels || null;
+
+    const taskDistributionData = [
+      { status: "Pending", count: taskDistribution?.Pending || 0 },
+      { status: "In Progress", count: taskDistribution?.InProgress || 0 },
+      { status: "Completed", count: taskDistribution?.Completed || 0 },
+    ];
+
+    setPieChartData(taskDistributionData);
+
+    const PriorityLevelData = [
+      { priority: "Low", count: taskPriorityLevels?.Low || 0 },
+      { priority: "Medium", count: taskPriorityLevels?.Medium || 0 },
+      { priority: "High", count: taskPriorityLevels?.High || 0 },
+    ]
+
+    setBarCharData(PriorityLevelData)
+  };
+
   const getDashboardData = async () => {
     try {
-      const response = await axiosInstance.get(API_PATHS.TASKS.GET_DASHBOARD_DATA)
-      if(response.data){
-        setDashboardData(response.data)
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA
+      );
+      if (response.data) {
+        setDashboardData(response.data);
+        prepareChartData(response.data?.charts || null)
       }
     } catch (error) {
-      console.error("Error fetching users:", error)
+      console.error("Error fetching users:", error);
     }
-  }
+  };
 
   const onSeeMore = () => {
-    navigate('/admin/tasks')
-  }
-  
+    navigate("/admin/tasks");
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getDashboardData();
     return () => {};
   }, []);
@@ -91,16 +119,36 @@ function DashBoard() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6 ">
+        <div>
+          <div className="card">
+            <div className="flex items-center justify-between">
+              <h5 className="font-medium">Task Distribution</h5>
+            </div>
+
+            <CustomPieChart data={pieChartData} colors={COLORS} />
+          </div>
+        </div>
+
+        <div>
+          <div className="card">
+            <div className="flex items-center justify-between">
+              <h5 className="font-medium">Task Priority Levels</h5>
+            </div>
+
+            <CustomBarChart data={barCharData} colors={COLORS} />
+          </div>
+        </div>
+
         <div className="md:col-span-2">
           <div className="card">
             <div className="flex item-center justify-between">
               <h5 className="text-lg">Recent Tasks</h5>
               <button className="card-btn" onClick={onSeeMore}>
-                See All <LuArrowRight className="text-base"/>
+                See All <LuArrowRight className="text-base" />
               </button>
             </div>
 
-            <TaskListTable tableData={dashboardData?.recentTasks || []}/>
+            <TaskListTable tableData={dashboardData?.recentTasks || []} />
           </div>
         </div>
       </div>
